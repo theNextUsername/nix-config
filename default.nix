21e2e4d35670dc42ac0258ddd8210cd14c835d1e
theNextUsername <thenextusername@thenextusername.xyz>
2025-01-05 19:20:52 -0500

    remove waybar

--- default.nix
+++ default.nix
@@ -16,7 +16,6 @@
   programs.hyprland.withUWSM = true;
   programs.hyprland.xwayland.enable = true;
   programs.uwsm.enable = true;
-  programs.waybar.enable = true;
 
   home-manager.useUserPackages = true;
   home-manager.useGlobalPkgs = true;
