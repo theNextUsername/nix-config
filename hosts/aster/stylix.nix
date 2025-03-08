761a75e4363d86288df8b502300166ac2a14a5f7
thenextusername <thenextusername@thenextusername.xyz>
2025-03-07 22:31:49 -0500

    Selected qualia theme

--- hosts/aster/stylix.nix
+++ hosts/aster/stylix.nix
@@ -3,7 +3,8 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/darcula.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/qualia.yaml";
     image = ./wallpaper.png;
+    homeManagerIntegration.followSystem = true;
   };
 }
