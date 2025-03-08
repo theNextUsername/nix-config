b642d9031ca6c7828a860bb72825932db1d54937
thenextusername <thenextusername@thenextusername.xyz>
2025-03-07 22:01:12 -0500

    Now it should

--- hosts/aster/stylix.nix
+++ hosts/aster/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "{pkgs.base16-schemes}/share/themes/darcula.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/darcula.yaml";
     image = ./wallpaper.png;
   };
 }
