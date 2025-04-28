40ff80e900e374501a217dd6f61fcbcd715c5d70
theNextUsername <thenextusername@thenextusername.xyz>
2025-04-28 08:03:30 -0400

    add explicit firewall enable

--- hosts/aster/networking.nix
+++ hosts/aster/networking.nix
@@ -4,8 +4,9 @@
     networking.networkmanager.enable = true;
     networking.networkmanager.dns = "dnsmasq";
 
+    networking.firewall.enable = true;
     networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
     networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
-    #networking.firewall.allowedTCPPorts = [  ];
+    networking.firewall.allowedTCPPorts = [  ];
     networking.firewall.allowedUDPPorts = [ 51820 ];
 }
