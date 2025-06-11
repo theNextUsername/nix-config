d2d8cf8d6e8f04665572adc63ecc4fa0b9a56bb5
theNextUsername <thenextusername@thenextusername.xyz>
2025-06-11 08:17:33 -0400

    Customize theme

--- hosts/aster/stylix.nix
+++ hosts/aster/stylix.nix
@@ -3,8 +3,27 @@
 {
   stylix = {
     enable = true;
-    polarity = "dark";
-#    base16Scheme = "${pkgs.base16-schemes}/share/themes/qualia.yaml";
+    base16Scheme = {
+      base00 = "061f1d";
+      base01 = "164f4a";
+      base02 = "347662";
+      base03 = "61ad7e";
+      base04 = "c7b972";
+      base05 = "dfea94";
+      base06 = "f2f4d2";
+      base07 = "f4f1db";
+      base08 = "d15e5e";
+      base09 = "579f73";
+      base0A = "699d69";
+      base0B = "759889";
+      base0C = "799a64";
+      base0D = "488467";
+      base0E = "a07ea0";
+      base0F = "539f7c";
+      author = "theNextUsername";
+      scheme = "Oneshot Maize";
+      slug = "maize";
+    };
     image = ./wallpaper.png;
     homeManagerIntegration.followSystem = true;
   };
