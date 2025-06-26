3e82ddd50eb8844d3d898cfbe1968e3652a20bf0
theNextUsername <thenextusername@thenextusername.xyz>
2025-06-26 16:44:26 -0400

    add xdg.mimeApps

--- users/tnu/home.nix
+++ users/tnu/home.nix
@@ -66,6 +66,17 @@
     };
   };
 
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+    };
+  };
+  
   fonts.fontconfig.enable = true;
 
   programs.git = {
