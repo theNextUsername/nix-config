04b150e44fcb062d0e540d88ea3e7e73f8df78b4
theNextUsername <thenextusername@thenextusername.xyz>
2025-07-03 10:12:16 -0400

    add waybar patch

--- users/tnu/waybar.nix
+++ users/tnu/waybar.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  programs.waybar.package = pkgs.waybar.overrideAttrs (prevAttrs: {
+    patches = [
+      (pkgs.fetchpatch {
+        url = "https://github.com/Alexays/Waybar/pull/4234.patch";
+        hash = "sha256-RRGy/aeFX95fW0pT6mXhww2RdEtoOnaT3+dc7iB3bAY=";
+      })
+    ];
+  });
   programs.waybar = {
     enable = true;
     systemd.enable = true;
