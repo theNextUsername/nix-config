52e2be5942bc4a4a699a45b7893746fca93ef455
theNextUsername <thenextusername@thenextusername.xyz>
2025-07-06 15:26:38 -0400

    edit monitor configs

--- users/tnu/niri.nix
+++ users/tnu/niri.nix
@@ -57,6 +57,18 @@
             { command = [ "${pkgs.kdePackages.kwallet-pam}/libexec/pam_kwallet_init" ]; }
           ];
           prefer-no-csd = true;
+          outputs."DP-1" = {
+            position = {
+              x = 1920;
+              y = 130;
+            };
+          };
+          outputs."HDMI-A-1" = {
+            position = {
+              x = 0;
+              y = 0;
+            };
+          };
           binds = with config.lib.niri.actions; {
             "Mod+Shift+Slash".action = show-hotkey-overlay;
             "XF86AudioRaiseVolume" = {
