abd98419d4f778c6fda74ba88ba5cf2d1aae2b63
theNextUsername <thenextusername@thenextusername.xyz>
2025-12-29 17:42:53 -0500

    centralize dnsmasq config

--- hosts/aster/networking.nix
+++ hosts/aster/networking.nix
@@ -3,8 +3,7 @@
     networking.hostName = "aster";
     networking.networkmanager.enable = true;
     networking.networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
-    networking.networkmanager.dns = "dnsmasq";
-
+    
     networking.firewall.enable = true;
     networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
     networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
--- hosts/sunflower/networking.nix
+++ hosts/sunflower/networking.nix
@@ -1,8 +1,5 @@
 { ... }:
 {
-    networking.networkmanager.enable = true;
-    networking.networkmanager.dns = "dnsmasq";
-
     networking.firewall.enable = true;
     networking.firewall.allowedTCPPorts = [ 22 ];
     networking.firewall.allowedUDPPorts = [ ];
