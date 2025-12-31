4fe7efda593607e586a37915e327c7eaa56a511c
theNextUsername <thenextusername@thenextusername.xyz>
2025-12-31 13:22:54 -0500

    add root ssh key

--- hosts/tuber/default.nix
+++ hosts/tuber/default.nix
@@ -10,5 +10,9 @@
     unison
   ];
 
+  users.users.root.openssh.authorizedKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID/CaNWRZR9TH3EIMU3lvN7IdBVps909rO4keGd7zTrT tnu@aster"
+  ];
+
   system.stateVersion = "25.11";
 }
