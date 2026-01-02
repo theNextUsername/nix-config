fa3a7df10a4c8aadf6b82efef551a25b87befdcc
theNextUsername <thenextusername@thenextusername.xyz>
2026-01-01 20:43:39 -0500

    fix boot loader?

--- modules/proxmox/configuration.nix
+++ modules/proxmox/configuration.nix
@@ -6,7 +6,7 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  boot.loader.grub.enable = true;
+  boot.loader.grub.enable = false;
   boot.kernelParams = [
     "console=ttyS0,115200"
   ];
