0d7f725739c9759069b77260eb591a871adeb4aa
theNextUsername <thenextusername@thenextusername.xyz>
2026-01-01 20:03:12 -0500

    move generation settings

--- flake.nix
+++ flake.nix
@@ -52,6 +52,20 @@
         inherit system;
         modules = [
           {
+            virtualisation.diskSize = 8 * 1024; # 8 GiB
+            proxmox = {
+              qemuConf = {
+                cores = 1;
+                memory = 2048;
+                name = "nixos";
+                agent = true;
+                net0 = "virtio=00:00:00:00:00:00,bridge=vmbr1,firewall=1";
+              };
+              cloudInit = {
+                enable = true;
+                defaultStorage = "vm-pool";
+              };
+            };
             system.stateVersion = "25.11";
           }
           ./modules/proxmox
--- modules/proxmox/configuration.nix
+++ modules/proxmox/configuration.nix
@@ -19,22 +19,6 @@
     ext4.enable = true;
   };  
 
-  virtualisation.diskSize = 8 * 1024; # 8 GiB
-
-  proxmox = {
-    qemuConf = {
-      cores = 1;
-      memory = 2048;
-      name = "nixos";
-      agent = true;
-      net0 = "virtio=00:00:00:00:00:00,bridge=vmbr1,firewall=1";
-    };
-    cloudInit = {
-      enable = true;
-      defaultStorage = "vm-pool";
-    };
-  };
-  
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzZsCPr9p5bdDz1wyhKelr+y8KtqlQDrzK63nWy1wzj tnu@aster"
