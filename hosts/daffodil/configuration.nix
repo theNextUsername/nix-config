f4f87a9a42b9302c12facf4ae1a4a93f13eb8a7a
theNextUsername <thenextusername@thenextusername.xyz>
2026-01-03 21:54:51 -0500

    add mollysocket to daffodil

--- hosts/daffodil/configuration.nix
+++ hosts/daffodil/configuration.nix
@@ -1,6 +1,11 @@
 { ... }:
 
 {
-
+  services.mollysocket.enable = true;
+  services.mollysocket = {
+    settings.allowed_endpoints = [
+      "https://push.services.mozilla.com"
+    ];
+  };
   system.stateVersion = "25.11";
 }
