aad67bda937e9600a6e2cd53a8c2111ccf9e252a
theNextUsername <thenextusername@thenextusername.xyz>
2026-01-25 20:19:22 -0500

    add TFTP firewall rules

--- hosts/aster/networking.nix
+++ hosts/aster/networking.nix
@@ -1,14 +1,27 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
     networking.hostName = "aster";
     networking.networkmanager.enable = true;
     networking.networkmanager.plugins = [ pkgs.networkmanager-openvpn ];
     
     networking.firewall.enable = true;
-    networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-    networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+    networking.firewall.allowedTCPPortRanges = [
+        { from = 1714; to = 1764; } # kdeconnect
+    ];
+    networking.firewall.allowedUDPPortRanges = [
+        { from = 1714; to = 1764; } # kdeconnect
+    ];
     networking.firewall.allowedTCPPorts = [  ];
     networking.firewall.allowedUDPPorts = [ 51820 ];
+    networking.firewall.connectionTrackingModules = [
+      "tftp"  
+    ];
+    networking.firewall.extraCommands = ''
+        iptables -A PREROUTING -t raw -j CT -p udp --dport 69 --helper tftp
+    '';
+    networking.firewall.extraStopCommands = ''
+        iptables -D PREROUTING -t raw -j CT -p udp --dport 69 --helper tftp
+    '';
     networking.extraHosts = ''
         10.0.0.1 owntracks.thenextusername.xyz
     '';
