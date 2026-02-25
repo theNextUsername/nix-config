9e18ddfe2913fdab1e523910006c1a5a479f1b80
theNextUsername <thenextusername@thenextusername.xyz>
2026-02-25 10:41:31 -0500

    Configure git editor as helix in tnu profile

--- users/tnu/home.nix
+++ users/tnu/home.nix
@@ -141,6 +141,7 @@ in
     settings = {
       user.name = "theNextUsername";
       user.email = "thenextusername@thenextusername.xyz";
+      core.editor = config.home.sessionVariables.EDITOR;
       credential.helper = "manager";
       credential."https://github.com".username = "theNextUsername";
       credential.credentialStore = "cache";
